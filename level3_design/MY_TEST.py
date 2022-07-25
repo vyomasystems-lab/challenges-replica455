cf60c257d6584cf60c2ee48fdeef5abc066a0382
SHIRSENDU BIKAS ACHARYYA <55652905+replica455@users.noreply.github.com>
2022-07-25 08:55:51 +0530

    Add files via upload

---
+++ level3_design/MY_TEST.py
@@ -0,0 +1,32 @@
+import random
+import sys
+import cocotb
+from cocotb.decorators import coroutine
+from cocotb.triggers import Timer, RisingEdge
+from cocotb.result import TestFailure
+from cocotb.clock import Clock
+
+from model_mkbitmanip import *
+
+# Clock Generation
+@cocotb.coroutine
+def clock_gen(signal):
+    while True:
+        signal.value <= 0
+        yield Timer(1) 
+        signal.value <= 1
+        yield Timer(1) 
+
+# Sample Test
+@cocotb.test()
+def run_test(dut):
+
+    # clock
+    cocotb.fork(clock_gen(dut.CLK))
+
+    # reset
+    dut.RST_N.value <= 0
+    yield Timer(10) 
+    dut.RST_N.value <= 1
+
+    ######### CTB : Modify the test to expose the bug #############
